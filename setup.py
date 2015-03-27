6cb440acbdc9279091c9b842b1155e4130bc9e37
Tristan Carel <tristan@cogniteev.com>
2015-03-27 14:42:28 +0100

    Repository now hosted under the cogniteev organization

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+NAME = 'pyonedrive'
+GITHUB_ORG_URL = "https://github.com/cogniteev"
+exec(open('pyonedrive/version.py').read())
+
 setup(
-    name='pyonedrive',
-    version='1.1.3',
+    name=NAME,
+    version=version,
     author='Tony Sanchez',
     author_email='mail.tsanchez@gmail.com',
-    url='https://github.com/tsanch3z/pyonedrive',
-    download_url='https://github.com/tsanch3z/pyonedrive/archive/master.zip',
+    url=GITHUB_ORG_URL + NAME,
+    download_url="{0}/{1}/tarball/v{2}".format(GITHUB_ORG_URL, NAME, version),
     description='Onedrive REST api client',
     packages=find_packages(exclude=['tests']),
     license='Apache license version 2.0',
     platforms='OS Independent',
+    zip_safe=False,
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
